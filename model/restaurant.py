a0d42a6579e7c8e9710bfbf0c432cc11d2830972
Pedro Itamaro <itamaropedro@gmail.com>
2024-03-10 20:49:06 -0300

    📦 FEAT: add protection and treatment to attributes of class restaurant

--- model/restaurant.py
+++ model/restaurant.py
@@ -1,22 +1,26 @@
-def list_restaurants():
-    for restaurante in Restaurant.restaurants:
-        print(f'{restaurante.name} | {restaurante.category} | {restaurante.status}')
-
-
 class Restaurant:
     restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.status = False
+        self._name = name.title()
+        self._category = category.upper()
+        self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
-        return f'{self.name} | {self.category}'
+        return f'{self._name} | {self._category} | Active: {self._active}'
 
+    @property
+    def active(self) -> str:
+        return "The store is operational" if self._active else "The store is not operational"
 
-restaurant_Pizza = Restaurant("Pizza Hut", "Pizza")
-restaurant_Sushi = Restaurant("Sushi Japex", "Sushi")
+    @classmethod
+    def list_restaurants(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)}|{'Categoria'.ljust(25)}|{'Status'.ljust(25)}')
+        print()
+        for restaurante in Restaurant.restaurants:
+            print(f'{restaurante._name.ljust(25)}|{restaurante._category.ljust(25)}|{restaurante.active.ljust(25)}')
 
-print(list_restaurants)
+    def alter_active(self):
+        self._active = not self._active
+
